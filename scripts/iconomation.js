773c5c768aac934df2a545f02ca4698a5ebd6ff7
Michael Oneppo <moneppo@gmail.com>
2014-10-08 15:52:23 -0400

    Iterating based on user testing.

--- scripts/iconomation.js
+++ scripts/iconomation.js
@@ -17,19 +17,38 @@ function calcCentered(index) {
 }
 
 $('.playspace').append('<i class="hidden em em-godmode"></i>');
+$('.playspace').append('<span class="message hidden">That\'s the code for god mode in DOOM!</span>');
 $('.em-godmode').css({
 	top: 200 - 96,
 	left: 300 - 32
-})
+});
+
+function fadeOut() {
+    $("#type-0").animate({opacity:"0.5"}, 500, fadeIn);
+}
+
+function fadeIn() {
+	 $("#type-0").animate({opacity:"1.0"}, 500, fadeOut);
+}
+
+
+fadeOut();
 
 var codeItem = 0;
 $(document).keydown(function(event) {
   var event = event || window.event;
   var letter = String.fromCharCode(event.keyCode).toLowerCase();
   if (letter == code[codeItem]) {
+		if (codeItem==0) {
+			$("#type-0").stop();
+			$('#type-0').css("opacity", 1.0);
+		}
     codeItem++;
     if (codeItem == code.length) {
       $('.em-godmode').animate({
+        opacity: 1.0
+      }, 1000);
+			$('.message').animate({
         opacity: 1.0
       }, 1000);
       $(document).off();
